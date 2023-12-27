a674ca43fc015e6914d1f2d4e26b3ac9ba9b4b61
patelrakesh <patevinod84@gmail.com>
2023-12-27 17:06:12 +0530

    add vercel file

--- middleware/advanceResults.js
+++ middleware/advanceResults.js
@@ -7,39 +7,30 @@ const advanceResults = (model, populate) => async (req, res, next) => {
     let results;
     const reqQuery = { ...req.query };
 
-    //Feild to exclude
-
     const removeFeilds = ["select", "sort", "limit", "page"];
 
     removeFeilds.forEach((param) => delete reqQuery[param]);
 
-    //Create query string
     let queryStr = JSON.stringify(reqQuery);
 
-    //Create operators ($gt,gte,lt,lte)
     queryStr = queryStr.replace(
       /\b(gt|gte|lt|lte|in)/g,
       (match) => `$${match}`
     );
 
-    //Finding resource
     query = model.find(JSON.parse(queryStr));
 
-    //Select
     if (req.query.select) {
       const select = req.query.select.split(",").join(" ");
       query = query.select(select);
     }
 
-    //Sort
     if (req.query.sort) {
       const sort = req.query.sort.split(",").join(" ");
       query = query.sort(sort);
     } else {
       query = query.sort("-createdAt");
     }
-
-    //Pagination
 
     if (req?.query?.page && req?.query?.limit) {
       const page = parseInt(req.query.page, 10) || 1;
@@ -54,10 +45,7 @@ const advanceResults = (model, populate) => async (req, res, next) => {
         query = query.populate(populate);
       }
 
-      //Execute Query
       results = await query;
-
-      // Pagination Result
 
       if (endIndex < total) {
         pagination.next = {
